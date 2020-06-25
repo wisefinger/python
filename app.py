b71725ce6bd3e8e28ff37b662e995f407c4e07ea
wisefinger <kris.cardoen@gmail.com>
2020-06-25 15:59:02 +0200

    Added bucketname generation

--- app.py
+++ app.py
@@ -13,13 +13,28 @@ def create_bucket_name(bucket_prefix):
     # The generated bucket name must be between 3 and 63 chars long
     return ''.join([bucket_prefix, str(uuid.uuid4())])
 
-# Generate new bucketname
-bucketname =  create_bucket_name("wise_")
-print(f'> new bucketname = {bucketname}')
+
+# Show current session settings
+print (f'> region name       :{boto3.session.Session().region_name}')
+print (f'> active profile    :{boto3.session.Session().profile_name}')
 
 
+# define function  for bucket creation
+def create_bucket(bucket_prefix, s3_connection):
+    session = boto3.session.Session()
+    current_region = session.region_name
+    bucket_name = create_bucket_name(bucket_prefix)
+    bucket_response = s3_connection.create_bucket(
+        Bucket=bucket_name,
+        CreateBucketConfiguration={
+        'LocationConstraint': current_region})
+    print(bucket_name, current_region)
+    return bucket_name, bucket_response
 
 
+# Create an actual bucket
+first_bucket_name, first_response = create_bucket(bucket_prefix='wisefinger',
+                                                  s3_connection=s3_resource.meta.client)
 
 
 
