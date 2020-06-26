c29bf82b310757e41f0743f549b374ce7cb45d61
wisefinger <kris.cardoen@gmail.com>
2020-06-26 12:50:07 +0200

    Added create tempfile action

--- app.py
+++ app.py
@@ -31,10 +31,23 @@ def create_bucket(bucket_prefix, s3_connection):
     print(bucket_name, current_region)
     return bucket_name, bucket_response
 
+# define function to generate test files
+def create_temp_file(size, file_name, file_content):
+    random_file_name = ''.join([str(uuid.uuid4().hex[:6]), file_name])
+    with open(random_file_name, 'w') as f:
+        f.write(str(file_content) * size)
+        print(f'> file create       :{random_file_name}')
+    return random_file_name
 
 # Create an actual bucket
-first_bucket_name, first_response = create_bucket(bucket_prefix='wisefinger',
-                                                  s3_connection=s3_resource.meta.client)
+# first_bucket_name, first_response = create_bucket(bucket_prefix='wisefinger',
+#                                                   s3_connection=s3_resource.meta.client)
+# Create a tempfile
+first_file_name = create_temp_file(300, '_testfile.txt', 'f')
+
+
+
+
 
 
 
